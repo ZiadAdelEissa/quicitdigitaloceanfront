00d827e4bed6d9971603d9d5ad1df964f4e12c83
ZiadAdelEissa <ziadadel12321@hotmail.com>
2025-08-06 16:29:39 +0300

    fixing url api's path to digtocean ip

--- carwash-frontend/src/Components/services/api.js
+++ carwash-frontend/src/Components/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://46.101.71.136/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://46.101.71.136:5000/api",
   withCredentials: true, // This ensures cookies are sent with requests
   headers: {
     "Content-Type": "application/json",
