acfe6ae13cbe7e3a8f1cda0a2c7ff66691e164e5
ZiadAdelEissa <ziadadel12321@hotmail.com>
2025-08-07 05:21:07 +0300

    add / to ocean ip

--- carwash-frontend/vite.config.js
+++ carwash-frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'http://46.101.71.136:5000',
+        target: 'http://46.101.71.136:5000/',
         changeOrigin: true,
         secure: false
       }
